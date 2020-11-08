9091ad02c0ac53641caa732b6c86d242df8201c1
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-11-08 15:25:58 +0000

    Update app1.py

--- apps/app1.py
+++ apps/app1.py
@@ -8,22 +8,8 @@ from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-from app import app
-covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-covid_df = covid_df[["iso_code",
-                    "continent",
-                     "location",
-                     "date",
-                     "total_cases",
-                    "new_cases",
-                    "new_cases_smoothed",
-                    "total_deaths",
-                    "new_deaths",
-                    "new_deaths_smoothed",]]
-covid_df = covid_df[covid_df["total_cases"].notna()]
-covid_df['m_date'] = pd.to_datetime(covid_df["date"], format='%Y-%m-%d').apply(lambda x: x.strftime('%Y-%m'))
-covid_df1 = covid_df[covid_df["iso_code"] != "OWID_WRL"]
-covid_df1 = covid_df1[covid_df1["date"].str.contains("-01")]
+from app import app, covid_df1
+
 months = covid_df.m_date.unique()
 months.sort()
 monthsdict ={}

dbd3c3f7088e685058d37d6bc57d3cf5cd06414e
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-11-08 15:27:09 +0000

    Update homepage.py

--- apps/homepage.py
+++ apps/homepage.py
@@ -5,20 +5,9 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from app import app
-# importing the current covid data as a csv file and cleaning it
-covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-covid_df = covid_df[["continent",
-                     "location",
-                     "date",
-                     "total_cases",
-                    "new_cases",
-                    "new_cases_smoothed",
-                    "total_deaths",
-                    "new_deaths",
-                    "new_deaths_smoothed",]]
+from app import app, covid_df
 
-covid_df = covid_df[covid_df["total_cases"].notna()]
+
 
 
 
